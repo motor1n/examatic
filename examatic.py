786b2c96f2efed120d150f1476d2ca54e52f8e35
Vladislav Motorin <motorin@yandexlyceum.ru>
2021-06-05 11:13:30 +0700

    Изменение app.run для запуска на Heroku

--- examatic.py
+++ examatic.py
@@ -333,5 +333,9 @@ if __name__ == '__main__':
     # Создаём экземпляр объекта "Билет":
     ticket = Ticket()
 
-    # Запускаем web-приложение:
-    app.run(host='localhost', debug=True)
+    # Запускаем web-приложение локально:
+    # app.run(host='localhost', debug=True)
+
+    # Запускаем web-приложение на Heroku:
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
